3fcbe6575aa61c59258866ea31d329db30eb8b52
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2022-12-16 23:00:57 +0530

    Added User Valid Email Program

---
+++ UC_3ValidEmail.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day19UserRegistrationSystem
+{
+    internal class UC_3ValidEmail
+    {
+        public static string REGEX_VALIDEMAIL = ("^[A-Za-z]{3,}([.][A-Za-z]{3,})?[@][A-Za-z]{2,}[.][A-Za-z]{2,}([.][A-Za-z]{2})?$");
+
+
+        public bool validateUC_3ValidEmail(string validemail)
+        {
+            return Regex.IsMatch(validemail, REGEX_VALIDEMAIL);
+        }
+    }
+}
