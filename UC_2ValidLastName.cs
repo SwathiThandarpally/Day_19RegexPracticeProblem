ed861a53a4792fb3819cb543451e0662a027120e
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2022-12-16 22:58:27 +0530

    Added User Last Program

---
+++ UC_2ValidLastName.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day19UserRegistrationSystem
+{
+    internal class UC_2ValidLastName
+    {
+        public static string REGEX_VALIDLASTNAME = (@"^[A-Z]{1}[A-Za-z]{2,}?$");
+
+
+        public bool validateUC_2ValidLastName(string validlastname)
+        {
+            return Regex.IsMatch(validlastname, REGEX_VALIDLASTNAME);
+        }
+    }
+}
