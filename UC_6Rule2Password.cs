44d67f675f6c4835f65aa06d3307ab6c6967c44d
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2022-12-16 23:10:04 +0530

    Added User Password Rule 2 Program

---
+++ UC_6Rule2Password.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day19UserRegistrationSystem
+{
+    internal class UC_6Rule2Password
+    {
+        public static string REGEX_PASSWORD = ("^[A-Z]{1,8}[a-z0-9]{7,}$");
+
+
+        public bool validatePassWord(string password2)
+        {
+            return Regex.IsMatch(password2, REGEX_PASSWORD);
+        }
+    }
+}
