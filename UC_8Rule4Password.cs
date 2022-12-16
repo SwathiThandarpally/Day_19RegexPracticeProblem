0d12f5d53c49e35d9517e2f4ee86b921b417e3c2
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2022-12-16 23:14:08 +0530

    Added User Password Rule 4 Program

---
+++ UC_8Rule4Password.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day19UserRegistrationSystem
+{
+    internal class UC_8Rule4Password
+    {
+        public static string REGEX_SPECIALCHARACTER = ("^[A-Za-z]{6,}[A-Z]{1,}[0-9]{1,}[-~!@#$%^*()_+{}:|?`;',]{1,}$");
+
+
+        public bool validateSpecialCharacter(string password4)
+        {
+            return Regex.IsMatch(password4, REGEX_SPECIALCHARACTER);
+        }
+    }
+}
